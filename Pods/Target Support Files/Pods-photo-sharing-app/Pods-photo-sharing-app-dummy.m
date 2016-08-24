931587cbed18b8448caf607230bf2997f6e9ced4
Tomomi <tomomi.tamura2486@gmail.com>
2016-08-24 16:03:46 -0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-photo-sharing-app/Pods-photo-sharing-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_photo_sharing_app : NSObject
+@end
+@implementation PodsDummy_Pods_photo_sharing_app
+@end
--- photo-sharing-app/ViewController.swift
+++ photo-sharing-app/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userName: UITextField!
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var password1: UITextField!
+    
+    @IBOutlet var password2: UITextField!
+    
+    @IBAction func loginPressed(sender: AnyObject) {
+        
+        self.password2.alpha = 0
+    }
+    
+
+    @IBAction func signupPressed(sender: AnyObject) {
+        
+        self.password2.alpha = 1
+    }
+    
+    
+    @IBAction func submitPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBOutlet var profileImage: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+ 
+        profileImage.translatesAutoresizingMaskIntoConstraints = true
+        
+        //Looks for single or multiple taps for keyborad to disaoear
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
-
+    
+    //for keyborad to disaoear
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
